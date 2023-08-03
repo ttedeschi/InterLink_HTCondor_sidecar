36f572635710b57adc87d26434add752b8aa4112
Tommaso Tedeschi <tedesk95@gmail.com>
2023-08-03 23:09:49 +0200

    Update dummy_curl_command.sh

--- dummy_curl_command.sh
+++ dummy_curl_command.sh
@@ -1,19 +1,76 @@
 curl -X POST -H "Content-Type: application/json" -d '{
-  "Pods": [
-    {
-      "ObjectMeta": {
-        "Name": "my-pod"
-      },
-      "Spec": {
-        "Containers": [
-          {
-            "Name": "my-container",
-            "Image": "nginx:latest",
-            "Command": ["echo", "Hello, HTCondor!"],
-            "Args": []
-          }
-        ]
-      }
-    }
-  ]
+    "Pods": [
+        {
+            "Pod": {
+                "ObjectMeta": {
+                    "Name": "my-pod-1",
+                    "Namespace": "my-namespace"
+                },
+                "Spec": {
+                    "Containers": [
+                        {
+                            "Name": "container-1",
+                            "Image": "nginx:latest",
+                            "Command": ["echo", "Hello, HTCondor!"],
+                            "Args": [],
+                            "VolumeMounts": [
+                                {
+                                    "Name": "config-map-vol",
+                                    "MountPath": "/etc/config"
+                                },
+                                {
+                                    "Name": "secret-vol",
+                                    "MountPath": "/etc/secret"
+                                }
+                            ]
+                        }
+                    ],
+                    "Volumes": [
+                        {
+                            "Name": "config-map-vol",
+                            "VolumeSource": {
+                                "ConfigMap": {
+                                    "Name": "my-config-map-1",
+                                    "Data": {
+                                        "key1": "value1",
+                                        "key2": "value2"
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            "Name": "secret-vol",
+                            "VolumeSource": {
+                                "Secret": {
+                                    "SecretName": "my-secret-1",
+                                    "Data": {
+                                        "username": "admin",
+                                        "password": "secretpassword"
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "Pod": {
+                "ObjectMeta": {
+                    "Name": "my-pod-2",
+                    "Namespace": "my-namespace"
+                },
+                "Spec": {
+                    "Containers": [
+                        {
+                            "Name": "container-2",
+                            "Image": "alpine:latest",
+                            "Command": ["echo", "Hello, Kubernetes!"],
+                            "Args": []
+                        }
+                    ]
+                }
+            }
+        }
+    ]
 }' http://localhost:8000/submit
